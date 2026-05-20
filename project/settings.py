2ee6e6d99f46abf6067e392401ebed8b20823749
Azeez Ololade <ololadeazeez.m@gmail.com>
2026-05-20 22:03:54 +0100

    updated debug

--- project/settings.py
+++ project/settings.py
@@ -25,11 +25,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&42&!k5c*0*3typ$)b)&^(py7v0up5jb8)d^lj(b6_#ng1e0kb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+from decouple import config
+
+# This pulls from Render's env, and defaults to False if it can't find it
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ["https://personal-dj-itrh.onrender.com"]
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
