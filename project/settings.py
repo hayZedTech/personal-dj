e9ab9bb07c979f036932c11dead415dedd2b1bf3
Azeez Ololade <ololadeazeez.m@gmail.com>
2026-05-22 08:46:45 +0100

    added cloudinary2

--- project/settings.py
+++ project/settings.py
@@ -38,16 +38,17 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(','
 # Application definition
 
 INSTALLED_APPS = [
-    "cloudinary_storage",
-    "cloudinary",
 
-    
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'cloudinary',
+    'django.contrib.staticfiles',
+    'cloudinary_storage',
 
 
     # "app",
@@ -155,6 +156,11 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 STORAGES = {
     "default":{
